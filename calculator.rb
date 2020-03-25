d67b0e3b325c4d3dce92e1998d1f4f2a55ed1770
Luca Kanyo <lucakanyo@protonmail.com>
2020-03-25 20:46:15 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,6 @@
-# Add your variables here
+first_number = 7
+second_number = 9
+sum = first_number + second_number
+difference = 7 - 9
+product = first_number * second_number
+quotient = first_number / second_number
